96cba0b7b7cdd4c6f57c0a6a408154b46cb797ca
Sadab Hafiz <sadabhafizny@gmail.com>
2023-02-05 18:48:20 -0500

    feat: connect backend to frontend (basic)

--- mtbi_predictor/mtbi_predictor/views.py
+++ mtbi_predictor/mtbi_predictor/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+from django.urls import reverse
 from rest_framework.views import APIView
 
 
@@ -8,7 +10,17 @@ class Predict(APIView):
     """
 
     def get(self, request):
-        """
-        Render `homepage.html` on get request
-        """
+        return render(request, "homepage.html")
+
+    def post(self, request):
+        query_text = str(request.data["query_text"])
+        using_default = bool(request.data.get("using_default", False))
+        response = requests.post(
+            request.build_absolute_uri(reverse("classification:classify-list")),
+            json={"query_text": query_text, "use_default": using_default},
+        )
+        if response.status_code == 200:
+            return render(
+                request, "homepage.html", {"prediction": response.json()["prediction"]}
+            )
         return render(request, "homepage.html")
